a1f7b31271f737f94458a584eba115d1f3492803
Nguyen Dinh Tao <ndtao2020@gmail.com>
2025-10-15 13:27:14 +0700

    Fix error: Can't import the named export __wbindgen_start

--- README.md
+++ README.md
@@ -24,13 +24,13 @@ chmod +x build.sh
 ### Publish to NPM
 
 ```sh
-cd pkg-bundler && npm publish
+cd pkg && npm publish
 ```
 
 ## Usage
 
 ```ts
-import { DiffieHellman, generate_keypair, X25519Keypair } from 'x25519-wasm-vn'
+import instantiate, { DiffieHellman, generate_keypair, X25519Keypair } from 'x25519-wasm-vn'
 
 interface KeyPair {
   privateKey: Uint8Array
@@ -40,6 +40,7 @@ interface KeyPair {
 export const generateKeyPair = async (): Promise<KeyPair> => {
   let keypair_wasm: X25519Keypair | null = null
   try {
+    await instantiate()
     keypair_wasm = generate_keypair()
     return {
       privateKey: keypair_wasm.private_key,
@@ -57,6 +58,7 @@ export const generateKeyPair = async (): Promise<KeyPair> => {
 export const diffieHellman = async (privateKey: Uint8Array, publicKey: Uint8Array): Promise<Uint8Array> => {
   let _wasm: DiffieHellman | null = null
   try {
+    await instantiate()
     _wasm = new DiffieHellman(privateKey)
     return _wasm.get_shared_key(publicKey)
   } catch (err) {
--- build.sh
+++ build.sh
@@ -6,17 +6,16 @@ echo "Building X25519-WASM..."
 
 cargo build --target wasm32-unknown-unknown --release
 
-wasm-bindgen --target web --out-dir ./pkg-web --omit-imports --reference-types ./target/wasm32-unknown-unknown/release/x25519_wasm_vn.wasm
+wasm-bindgen --target web --out-dir ./pkg ./target/wasm32-unknown-unknown/release/x25519_wasm_vn.wasm
+wasm-bindgen --target deno --out-dir ./pkg/deno ./target/wasm32-unknown-unknown/release/x25519_wasm_vn.wasm
+wasm-bindgen --target nodejs --out-dir ./pkg/nodejs ./target/wasm32-unknown-unknown/release/x25519_wasm_vn.wasm
+wasm-bindgen --target bundler --out-dir ./pkg/bundler ./target/wasm32-unknown-unknown/release/x25519_wasm_vn.wasm
 
-wasm-bindgen --target deno --out-dir ./pkg-deno --omit-imports --reference-types ./target/wasm32-unknown-unknown/release/x25519_wasm_vn.wasm
+cp package.json pkg/package.json
+cp README.md pkg/README.md
+cp LICENSE pkg/LICENSE
 
-wasm-bindgen --target nodejs --out-dir ./pkg-nodejs --omit-imports --reference-types ./target/wasm32-unknown-unknown/release/x25519_wasm_vn.wasm
-
-wasm-bindgen --target bundler --out-dir ./pkg-bundler --omit-imports --reference-types ./target/wasm32-unknown-unknown/release/x25519_wasm_vn.wasm
-
-cp package.json pkg-bundler/package.json
-cp README.md pkg-bundler/README.md
-cp LICENSE pkg-bundler/LICENSE
+echo "*" > ./pkg/.gitignore
 
 # Optimize WASM
 echo "⚡ Optimizing WASM..."
